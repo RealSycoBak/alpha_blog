2b9c19f15dac71fbd98c073675eeba9e4b15e1f6
Alon Baker <66329736+RealSycoBak@users.noreply.github.com>
2024-12-04 21:45:51 -0500

    more user updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @users, notice: "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up." }
+        format.html { redirect_to @user, notice: "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @users, notice: "Your account was successfully updated." }
+        format.html { redirect_to @user, notice: "Your account was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
